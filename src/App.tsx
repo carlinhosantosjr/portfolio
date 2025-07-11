fec98d7f9f33e761171d30540c4acfc76497c78e
carlinhosantosjr <carlinhosantosjr@hotmail.com>
2025-07-11 16:06:52 -0300

    feat: Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { Header } from './components/Header'
+
 export function App() {
   return (
-    <div>
-      Hello
+    <div className="bg-[var(--background-darker)] min-w-fit min-h-screen">
+      <div className="bg-[var(--background-maincard)] pt-6 max-w-[976px] m-auto">
+        <Header />
+      </div>
     </div>
 
   )
---
+++ src/components/Header.tsx
@@ -0,0 +1,52 @@
+import avatar from '../assets/avatar.png'
+import flag from '../assets/flag.png'
+import react from '../assets/react-icon.png'
+
+export function Header() {
+  return (
+    <div className="grid grid-cols-[auto_1fr_268px] max-w-5xl min-h-56 pt-2.5 pl-6 pr-6">
+      <img
+        src={avatar}
+        alt="profile"
+        className="max-w-40 border-1 border-[var(--background-darker)] rounded-sm"
+      />
+
+      <div className="pl-9 pr-4">
+        <div className="flex">
+          <h2 className="text-[var(--fontcolor-white)] text-2xl">
+            carlinhosantosjr
+          </h2>
+        </div>
+
+        <div className="flex text-[var(--fontcolor-lightgray)] text-[0.813rem] gap-0.5 ">
+          <span className="pr-2 truncate">Carlos Antonio</span>
+          <img
+            src={flag}
+            alt="brasil"
+            className="max-w-4 max-h-3 self-center"
+          />
+          <span className=" ">Batatais, São Paulo, Brasil</span>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-3">
+        <div className="flex text-center gap-1.5">
+          <h2 className="text-2xl text-[var(--fontcolor-white)] flex">Nível</h2>
+          <span
+            className="border-2 rounded-full w-7 h-7 border-blue-500
+           text-[var(--fontcolor-white)]"
+          >JR
+          </span>
+        </div>
+
+        <div className="flex  h-[70px] bg-[var(--background-badge)] gap-2 text-[var(--fontcolor-white)] items-center pl-2 rounded-sm shadow-black shadow-xs">
+          <img src={react} alt="react-icon" className="h-[54px] w-[54px] rounded-xl shadow-black shadow-m" />
+          <div className="flex flex-col">
+            <span className="text-sm">React</span>
+            <span className="text-xs text-[var(--fontcolor-lightgray)]">Avançado</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
